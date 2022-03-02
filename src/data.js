71deb3147c876a4c9dcf576cea4cc7c705c672df
Jerysha <jerysha.s.a@gmail.com>
2022-03-01 18:04:10 -0600

    test

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const questions = [
+    {
+      id: 1,
+      title: 'Which are the areas of data structures?',
+      info:
+        ' numerical analysis, OS, AI, compiler style, management, artificial intelligence, simulation, DBMS (Database Management System), operating system, graphs and applied mathematical analysis, etc.',
+    },
+    {
+      id: 2,
+      title: 'What is LIFO?',
+      info:
+        '  is a method used to account for inventory that records the most recently produced items as sold first. Under LIFO, the cost of the most recent products purchased (or produced) are the first to be expensed as cost of goods sold (COGS), which means the lower cost of older products will be reported as inventory. and its opposite called FIFO. ',
+    },
+    {
+      id: 3,
+      title: 'What is a queue?',
+      info:
+        'is a linear structure which follows a particular order in which the operations are performed. The order is First In First Out (FIFO). A good example of a queue is any queue of consumers for a resource where the consumer that came first is served first. The difference between stacks and queues is in removing. In a stack we remove the item the most recently added; in a queue, we remove the item the least recently added.',
+    },
+    {
+      id: 4,
+      title: 'What are binary trees?',
+      info:
+        'A binary tree has 2 nodes, a left node, and a right node. In programming, binary trees are continuations of coupled list structures. A binary tree in which each parent node can have at most two children. Each node of a binary tree consists of three items:  1. Data item,  2. Address of left child, 3. Address of right child ',
+    },
+    {
+      id: 5,
+      title: 'To manage an algorithmic function, what data structures are used?',
+      info:
+        'Recursion is a data structure used to manage an algorithmic function, as it is used in the stack based on a final condition.',
+    },
+  ]
+
+  export default questions
